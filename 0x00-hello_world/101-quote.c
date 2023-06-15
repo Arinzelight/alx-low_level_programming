2f33f0c6344ab2d9fefa5fc57da9cc3aa189c5bb
Arinzelight <arinzelight2@gmail.com>
2023-06-15 16:25:14 -0700

    print a line using fputs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints set of characters followed by new line using fput
+ * main - A program that prints a line using fputs
  *
  * Return: 1 (Success)
  */
