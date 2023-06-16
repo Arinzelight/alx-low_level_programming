0fabe340d1b61dd9d6732016a753927f89b4257d
Arinzelight <arinzelight2@gmail.com>
2023-06-16 16:36:12 -0700

    three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,26 +6,28 @@
  */
 int main(void)
 {
-    int a, b, c, d;
+    int a, b, c;
 
-    for (a = 0; a < 10; a++)
+    for (a = 0; a < 8; a++)
     {
-        for (b = a + 1; b < 10; b++)
+        for (b = a + 1; b < 9; b++)
         {
             for (c = b + 1; c < 10; c++)
             {
-                for (d = c + 1; d < 10; d++)
+                putchar(a + '0');
+                putchar(b + '0');
+                putchar(c + '0');
+
+                if (!(a == 7 && b == 8 && c == 9))
                 {
-                    putchar(a + '0');
-                    putchar(b + '0');
-                    putchar(c + '0');
-                    putchar(d + '0');
-                    putchar(d == 9 && c == 8 && b == 7 && a == 6 ? '\n' : ',');
-                    putchar(d == 9 && c == 8 && b == 7 && a == 6 ? '\0' : ' ');
+                    putchar(',');
+                    putchar(' ');
                 }
             }
         }
     }
+
+    putchar('\n');
 
     return (0);
 }
