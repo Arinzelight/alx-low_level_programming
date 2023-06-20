bfee7dd961be759b8cad9ed1804990d5c8ebab19
Arinzelight <arinzelight2@gmail.com>
2023-06-20 11:30:48 -0700

    function to print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
+
 /**
-* function that prints 10 times the alphabet,lowercase,
-* Return: void
+* print_alphabet_x10 - Prints the alphabet in lowercase 10 times
 */
 
 void print_alphabet_x10(void)
