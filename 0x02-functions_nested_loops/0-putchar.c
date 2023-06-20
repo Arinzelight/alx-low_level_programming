9365dfa7a322d6cba25601c28124eebc6c9a5f8f
Arinzelight <arinzelight2@gmail.com>
2023-06-20 09:13:18 -0700

    C program using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - program to print _putchar using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
