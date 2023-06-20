99a579644b912c6a539a5ac348232d95564cdfb3
Arinzelight <arinzelight2@gmail.com>
2023-06-20 16:16:13 -0700

    9x table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,27 +5,28 @@
 */
 void times_table(void)
 {
-	int i, j;
-
-	for (i = 0; i <= 9; i++)
+	int i, j, k;
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			int result = i * j;
-			if (result < 10)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar('0' + result);
+				_putchar(k + '0');
 			}
-			else
-			{
-				_putchar('0' + result / 10);
-				_putchar('0' + result % 10);
-			}
-			if (j < 9)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
-			_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
