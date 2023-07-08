adaaccf44291afce18497b1e326d559212a593eb
Arinzelight <arinzelight2@gmail.com>
2023-07-07 20:01:03 -0700

    static libraries

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a quote by Kevin Mitnick using the _puts
+ *              function. It returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+	  "\t- Kevin Mitnick");
+	return (0);
+}
